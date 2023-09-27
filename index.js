b0dd1ca80ccc6a06057701c7643faae8f44f0dbe
Velma Kimani <stanvelma16@gmail.com>
2023-09-27 09:54:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+function findMatching(drivers, string){
+    console.log(drivers);
+    return drivers.filter(function (driver) { 
+        return driver.toLowerCase() === string.toLowerCase()
+});
+    // let newDrivers = [];
+    // for(let driver of drivers){
+    //     if(driver === string || driver.toLowerCase() === string.toLowerCase()){
+    //         console.log(driver);
+    //         newDrivers.push(driver);
+    //     }
+    // }
+    // return newDrivers;
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver){
+        return driver[0][0] === string[0][0] });
+      //  return driver.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(function(driver){
+        return driver.name === string;
+    });
+    
+}
